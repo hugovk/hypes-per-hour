6125c8e5adf22dbb2cd153959a6cae445794131a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-30 09:28:42 +0200

    Search for comma-separated --words, print as table, save to CSV and TSV

--- countem.py
+++ countem.py
@@ -3,19 +3,10 @@
 
 import argparse
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="TODO", formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument("-v", "--video", default="609400064", help="Video ID")
-    parser.add_argument("-w", "--word", default="hype", help="Word to search for")
-    args = parser.parse_args()
+from prettytable import PrettyTable
 
-    with open(f"data/{args.video}.txt") as f:
-        lines = f.readlines()
 
-    search_word = args.word
-
+def do_word(search_word: str, lines: list) -> list:
     def get_hour(line):
         elapsed = line[1:].split("]")[0]
         elapsed = elapsed.replace("1 day, 0", "24")
@@ -39,8 +30,39 @@ if __name__ == "__main__":
             hour += 1
             hypes_per_hour.append(hypes)
 
-    print(f"hour\t{search_word}s per hour ({search_word[0].upper()}PH)")
-    # print(f"{search_word}s per hour ({search_word[0].upper()}PH)")
-    for i, thing in enumerate(hypes_per_hour):
-        print(f"{i+1}\t{thing}")
-        # print(f"{thing}")
+    return hypes_per_hour
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="TODO", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-v", "--video", default="609400064", help="Video ID")
+    parser.add_argument("-w", "--words", default="hype", help="CSV words to search for")
+    args = parser.parse_args()
+
+    with open(f"data/{args.video}.txt") as f:
+        lines = f.readlines()
+
+    results = dict()
+    for search_word in args.words.split(","):
+
+        words_per_hour = do_word(search_word, lines)
+        results[search_word] = words_per_hour
+
+    table = PrettyTable()
+    first_thing = next(iter(results))
+    # Start from 1
+    hours = list(range(1, len(results[first_thing]) + 1))
+    table.add_column("hour", hours)
+
+    for word, counts in results.items():
+        table.add_column(word, counts)
+
+    print(table)
+
+    with open("data/output.csv", "w") as f:
+        print(table.get_csv_string(), file=f)
+
+    with open("data/output.tsv", "w") as f:
+        print(table.get_csv_string(delimiter="\t"), file=f)
